511cdbb8665d396a19f1f273b17210c6a5f84e3a
Rizwan Ashraf <riz1ash786@gmail.com>
2022-03-11 20:20:29 +0000

    storedata nodemon restarted successfully on port 3000

--- db/storeData.js
+++ db/storeData.js
@@ -1,17 +1,46 @@
 // require dependencies
 const fs = require("fs");
 const util = require("util");
+const uuid = require("uuid");
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
+class Save {
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
 
+  addNote(note) {
+    const { title, text } = note;
 
+    if (!title || !text) {
+      throw new Error("title and text cannot be blank");
+    }
+    // UUID used to create IDs
+    const newNote = { title, text, id: uuid() };
 
-
-
-
-// Delete request function
-clearNote(id) {
     return this.getNotes()
-        .then(notes => notes.filter(note => note.id !== id))
-        .then(keptNotes => this.write(keptNotes))
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => this.newNote);
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      return JSON.parse(notes) || [];
+    });
+  }
+
+  // Delete request function
+  clearNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((keptNotes) => this.write(keptNotes));
+  }
 }
+
+module.exports = new Save();
