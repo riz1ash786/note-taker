935d5c11e869cbf44ff12a651cb3db96c457c7a4
Rizwan Ashraf <riz1ash786@gmail.com>
2022-03-11 20:07:33 +0000

    get requests for html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,4 +2,14 @@
 const router = require("express").Router();
 const path = require("path");
 
-//
+//GET request returns notes.html
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+//defaults to homepage if no matching route found
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
