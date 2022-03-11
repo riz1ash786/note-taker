e9ac987f2491f8087fd44e1015553a10595a0e55
Rizwan Ashraf <riz1ash786@gmail.com>
2022-03-11 18:08:24 +0000

    post request in apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const store = require("../db/storeData");
 
 // GET request
-
 router.get("/notes", (req, res) => {
   store
     .getNotes()
@@ -14,6 +13,17 @@ router.get("/notes", (req, res) => {
       res.status(500).json(err);
     });
 });
+
 // POST request
+router.post("/notes", (req, res) => {
+  storeData
+    .addNote(req.body)
+    .then((note) => {
+      res.json(note);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 //
