45c3c28ef124c2c04b9fd8e7448b0da0e4889a9a
Rizwan Ashraf <riz1ash786@gmail.com>
2022-03-11 17:53:44 +0000

    set up parse and listener

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ const path = require("path");
 // express server create at server port 3000
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// parse date from string or array
+app.use(express.urlencoded({ extended: true }));
+
+// parse json data
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// event listener
+app.listen(PORT, () => {
+  console.log(`Now listening on PORT: ${PORT}`);
+});
